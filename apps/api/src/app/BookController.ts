a060e2d5fe699bc9e9f4ac4e24111056c02bbd61
Thada Wangthammang <mildronize@gmail.com>
2021-12-15 14:08:52 +0700

    update type

--- apps/api/src/app/BookController.ts
+++ apps/api/src/app/BookController.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { controller, httpGet } from '@mildjs/router';
 import BookService from './BookService';
 
@@ -8,12 +8,9 @@ export default class BookController {
   constructor(private bookService: BookService) { }
 
   @httpGet()
-  public async getBooks(req: any, res: any, next: any) {
+  public async getBooks(req: Request, res: Response, next: NextFunction) {
       const data = this.bookService.getBooks();
       res.status(200).json( data );
   }
 
-  // routes() {
-  //   return Router().get('/', (_req, res) => res.send(this.getBooksRoute()));
-  // }
 }
--- libs/router/src/lib/decorators/use.ts
+++ libs/router/src/lib/decorators/use.ts
@@ -1,13 +1,13 @@
 import { getMetadataArgsStore } from './metadata';
-
+import { AnyFunction } from '../types';
 export function Use(
   // tslint:disable-next-line:array-type
-  ...middlewares: Array<Function | ((request: any, response: any, next: Function) => any)>
-): Function {
-  return (objectOrFunction: Object | Function, methodName?: string) => {
+  ...middlewares: Array<AnyFunction | ((request: any, response: any, next: AnyFunction) => any)>
+): AnyFunction {
+  return (objectOrFunction: Object | AnyFunction, methodName?: string) => {
     middlewares.forEach((middleware) => {
       getMetadataArgsStore().middlewares.push({
-        target: methodName ? objectOrFunction.constructor : (objectOrFunction as Function),
+        target: methodName ? objectOrFunction.constructor as AnyFunction : (objectOrFunction as AnyFunction),
         methodName: methodName || '',
         middleware,
       });
